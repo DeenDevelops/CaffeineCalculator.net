376456bd83a2806539cb440264b6df78fa32369b
Deen Knotts <deen.knotts@gmail.com>
2023-09-05 18:15:07 -0500

    json file updated

--- javascript/calculator.js
+++ javascript/calculator.js
@@ -1,4 +1,4 @@
-fetch('../json/bevs.json')
+fetch('../json/bevs2.0.json')
     .then(response => response.json())
     .then(data => {
         const selectElement = document.getElementById('beverageSelect');
@@ -45,6 +45,33 @@ document.getElementById('beverageForm').addEventListener('submit', function (eve
         console.error('Invalid caffeine amount');
     }
 });
+
+// Listen for the "input" event on the search input field
+document.getElementById('searchInput').addEventListener('input', function(event) {
+    const searchTerm = event.target.value.toLowerCase(); // Convert search term to lower case
+    const suggestionBox = document.getElementById('suggestionBox');
+    suggestionBox.innerHTML = ''; // Clear previous suggestions
+
+    // Filter beverage list based on search term
+    const matchingBeverages = beveragesArray.filter(beverage => 
+        beverage.name.toLowerCase().includes(searchTerm)
+    );
+
+    // Create and show new suggestions
+    matchingBeverages.forEach(beverage => {
+        const suggestion = document.createElement('div');
+        suggestion.textContent = beverage.name;
+        suggestion.className = 'suggestion'; // for styling
+        suggestion.addEventListener('click', function() {
+            document.getElementById('searchInput').value = beverage.name; // Auto-fill on click
+            suggestionBox.innerHTML = ''; // Clear suggestions
+        });
+        suggestionBox.appendChild(suggestion);
+    });
+});
+
+// Existing code for form submission
+// ...
 
 document.getElementById('beverageForm').addEventListener('submit', function (event) {
     event.preventDefault(); // Prevent the form from being submitted
